178f3f4e1f9108827a3e72af7ccf21d3f80963a5
ISTC\ITSC <kaiser849@gmail.com>
2025-10-28 19:46:45 +0900

    초기화

---
+++ src/main/java/it/exam/backoffice/common/utils/TimeFortmatUtils.java
@@ -0,0 +1,35 @@
+package it.exam.backoffice.common.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class TimeFormatUtils {
+
+    private static  final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static String getNowTime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    }
+
+    /**
+     * LocalDateTime -> String
+     * @param localDateTime
+     * @return
+     */
+    public static String getTime(LocalDateTime localDateTime, String pattern) {
+        pattern = pattern == null ? DATE_FORMAT : pattern;
+        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * LocalDateTime -> String
+     * @param localDateTime
+     * @return
+     */
+    public static String getTime(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    }
+}
